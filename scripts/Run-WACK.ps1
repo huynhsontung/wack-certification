1237f6d7bf79fefed5cba660265d766ecadaf4b7
Tung Huynh <tung75605@gmail.com>
2023-09-07 01:34:06 -0700

    set peruser appusage

--- scripts/Run-WACK.ps1
+++ scripts/Run-WACK.ps1
@@ -32,7 +32,7 @@ Write-Verbose "packagepath = $packagePath"
 $reportoutpath = "$reportoutputdirectory\$reportName"
 Write-Output "reportPath=$reportoutpath" >> $env:GITHUB_OUTPUT
 
-$arguments = "-resetTesting -appxpackagepath `"$packagePath`" -reportoutputpath `"$reportoutpath`" -apptype `"$appType`""
+$arguments = "-appusage peruser -appxpackagepath `"$packagePath`" -reportoutputpath `"$reportoutpath`" -apptype `"$appType`""
 if ($testIds.Count -gt 0) {
     $arguments += " -testid [$($testIds -join ',')]"
 }
